23b21c820b852ffd2d9e27bee7c0971721de521b
SilentDepth <silentdepth@outlook.com>
2020-03-31 15:49:38 +0800

    home page (wireframe)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app" class="min-h-screen bg-gray-900 text-gray-300 flex flex-col">
+    <div class="flex-none h-header bg-black text-gray-500">
+      <header class="w-page h-header mx-auto flex">
+        <strong class="self-center font-medium">毛玉線圈物語 Player Stats</strong>
+        <span class="self-center w-6 h-6 ml-auto bg-cool-gray-800"></span>
+      </header>
+    </div>
+    <div class="border-b border-gray-800" style="height: 300px;"></div>
+    <div class="border-b border-gray-800">
+      <div class="w-page mx-auto py-4 flex">
+        <span class="h-4 w-40 bg-cool-gray-800"></span>
+        <span class="h-4 w-16 ml-auto bg-cool-gray-800"></span>
+      </div>
+    </div>
+    <div class="w-page mx-auto py-5">
+      <div class="flex flex-wrap -ml-5 -mt-5">
+        <div v-for="n of 40" :key="n" class="flex-grow-0 flex-shrink w-1/4 pl-5 mt-5">
+          <div class="p-4 border border-gray-800 rounded-md flex">
+            <div class="w-16 h-16 border border-cool-gray-800 rounded-md"></div>
+          </div>
+        </div>
+      </div>
+      <div class="mt-5 py-4 border border-gray-800 rounded-md text-center">
+        <span class="inline-block h-4 w-20 bg-cool-gray-800"></span>
+      </div>
+    </div>
+    <div class="flex-none mt-auto border-t border-gray-800">
+      <footer class="w-page mx-auto py-5 flex items-center">
+        <p class="ml-auto text-sm text-gray-600">&copy; 毛玉線圈物語</p>
+      </footer>
+    </div>
   </div>
 </template>
 
@@ -20,14 +48,3 @@
 </script>
 
 <style src="./assets/css/base.scss" lang="scss"></style>
-
-<style lang="scss">
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  theme: {
+    extend: {
+      spacing: {
+        page: '1200px',
+        header: '50px',
+      },
+    },
+  },
   plugins: [
     require('@tailwindcss/ui'),
   ],

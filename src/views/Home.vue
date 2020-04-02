30aa7db7afbccf797967f68a8da44302da24c0a8
SilentDepth <silentdepth@outlook.com>
2020-04-03 01:40:06 +0800

    fix: accessing property of null

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <div>
         <h1 class="mb-5 text-5xl font-black">{{ infoJson?.servername }}</h1>
         <p>
-          <a :href="infoJson.homepage" target="_blank" class="text-blue-500 inline-flex items-center">
+          <a :href="infoJson?.homepage" target="_blank" class="text-blue-500 inline-flex items-center">
             <span class="text-xl">去看看</span>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 h-6 ml-2 fill-current">
               <path d="M10 6v2H5v11h11v-5h2v6a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h6zm11-3v8h-2V6.413l-7.793 7.794-1.414-1.414L17.585 5H13V3h8z"/>

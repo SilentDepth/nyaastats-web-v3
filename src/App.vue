dbbaa5e5125688c2b140fca374478d78dedfddfd
SilentDepth <silentdepth@outlook.com>
2020-04-03 01:09:16 +0800

    few effects for search box

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
     <div class="flex-none h-header bg-black text-gray-500">
       <header class="w-page h-header mx-auto flex">
         <RouterLink to="/" class="self-center font-medium">{{ infoJson?.title }}</RouterLink>
-        <span class="self-center w-6 h-6 ml-auto bg-cool-gray-800"></span>
+        <button class="ml-auto flex items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 h-6 fill-current text-cool-gray-500">
+            <path d="M12.866 3l9.526 16.5a1 1 0 0 1-.866 1.5H2.474a1 1 0 0 1-.866-1.5L11.134 3a1 1 0 0 1 1.732 0zm-8.66 16h15.588L12 5.5 4.206 19zM11 16h2v2h-2v-2zm0-7h2v5h-2V9z" />
+          </svg>
+        </button>
       </header>
     </div>
     <RouterView />
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -27,20 +27,34 @@
   <div class="border-b border-gray-800">
     <label class="block cursor-pointer">
       <span class="block w-page mx-auto flex">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="flex-none self-center w-6 h-6 fill-current">
-          <path d="M18.031 16.617l4.283 4.282-1.415 1.415-4.282-4.283A8.96 8.96 0 0 1 11 20c-4.968 0-9-4.032-9-9s4.032-9 9-9 9 4.032 9 9a8.96 8.96 0 0 1-1.969 5.617zm-2.006-.742A6.977 6.977 0 0 0 18 11c0-3.868-3.133-7-7-7-3.868 0-7 3.132-7 7 0 3.867 3.132 7 7 7a6.977 6.977 0 0 0 4.875-1.975l.15-.15z" />
-        </svg>
-        <input v-model="keyword" type="text" class="flex-1 mr-4 py-4 pl-8 -ml-6 bg-transparent" placeholder="搜索玩家">
+        <button class="flex-none flex items-center relative" @click="() => {keyword && (keyword = null); $refs.searchInput.focus()}">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+            <path v-if="!keyword" d="M18.031 16.617l4.283 4.282-1.415 1.415-4.282-4.283A8.96 8.96 0 0 1 11 20c-4.968 0-9-4.032-9-9s4.032-9 9-9 9 4.032 9 9a8.96 8.96 0 0 1-1.969 5.617zm-2.006-.742A6.977 6.977 0 0 0 18 11c0-3.868-3.133-7-7-7-3.868 0-7 3.132-7 7 0 3.867 3.132 7 7 7a6.977 6.977 0 0 0 4.875-1.975l.15-.15z" />
+            <path v-else d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-11.414L9.172 7.757 7.757 9.172 10.586 12l-2.829 2.828 1.415 1.415L12 13.414l2.828 2.829 1.415-1.415L13.414 12l2.829-2.828-1.415-1.415L12 10.586z" />
+          </svg>
+        </button>
+        <input
+          ref="searchInput"
+          v-model="keyword"
+          type="text"
+          placeholder="搜索玩家"
+          class="flex-1 mr-4 py-4 pl-8 -ml-6 bg-transparent"
+          @focus="isSearchInputFocused = true"
+          @blur="isSearchInputFocused = false"
+        >
         <button class="flex-none ml-auto text-cool-gray-500">随机查看</button>
       </span>
     </label>
   </div>
   <div class="w-page mx-auto py-5">
-    <p class="mb-5">{{ keyword ? '匹配的玩家' : '过去 24 小时内在线的玩家'}}：{{ keyword ? searchResult.length : recentPlayers.length }}</p>
-    <KeepAlive>
-      <PlayerList v-if="!keyword" :data="recentPlayers" />
-    </KeepAlive>
-    <PlayerSearchResult v-if="keyword" :data="searchResult" />
+    <div v-show="!keyword" :class="['transition-opacity duration-200 ease-linear', {'opacity-50': isSearchInputFocused}]">
+      <p class="mb-5">过去 24 小时内在线的玩家：{{ recentPlayers.length }}</p>
+      <PlayerList :data="recentPlayers" />
+    </div>
+    <div v-if="keyword">
+      <p class="mb-5">匹配的玩家：{{ searchResult.length }}</p>
+      <PlayerSearchResult v-if="keyword" :data="searchResult" />
+    </div>
   </div>
 </template>
 
@@ -100,6 +114,7 @@
       })
 
       const keyword: Ref<string | null> = ref(process.env.NODE_ENV === 'development' ? 'cc' : null)
+      const isSearchInputFocused = ref(false)
 
       const searchResult = computed(() => {
         if (!players.value || !keyword.value) return []
@@ -120,8 +135,10 @@
         worldAge,
 
         players,
-        keyword,
         recentPlayers,
+
+        keyword,
+        isSearchInputFocused,
         searchResult,
       }
     },
